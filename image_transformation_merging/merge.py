d016f820c4604c0b6f0297063495e5bb9de954dc
Kletzander <kletzander@cip.ifi.lmu.de>
2019-02-21 19:24:15 -0500

    by-pixel iteration working, checkpoint before by-triangle iteration

--- image_transformation_merging/merge.py
+++ image_transformation_merging/merge.py
@@ -69,7 +69,6 @@ def calc_affine_transform(triangle1, triangle2):
     transform = np.append(transform, [0.0, 0.0, 1.0])
     transform = np.reshape(transform, (3,3))
 
-    #print(np.matmul(transform, to_homog(triangle1[0])))
     return transform
 
 def create_triangles(points, indices):
@@ -77,7 +76,6 @@ def create_triangles(points, indices):
     for i in range(len(indices)):
        triangle = [points[indices[i][0]], points[indices[i][1]], points[indices[i][2]]]
        triangles.append(triangle)
-    print(triangles[0])
     return np.array(triangles)
 
 def calc_transform_all(triangles1, triangles2):
@@ -90,79 +88,94 @@ def calc_transform_all(triangles1, triangles2):
 def calc_origin_pixel_color(original_img, pixel_coords, transform_origtoavg):
     inverse_transform = np.linalg.inv(transform_origtoavg)
     original_px = from_homog(np.matmul(inverse_transform, to_homog(pixel_coords)))
-    px_color = original_img[int(np.round(original_px[1])), int(np.round(original_px[0])), :]
+    orig_x = int(round(original_px[0]))
+    orig_y = int(round(original_px[1]))
+
+    #handle edges of image where translated index is out of bounds
+    if orig_x >= original_img.shape[0]:
+        orig_x = original_img.shape[0]-1
+    elif orig_x < 0:
+        orig_x = 0
+    if orig_y >= original_img.shape[1]:
+        orig_y = original_img.shape[1]-1
+    elif orig_y < 0:
+        orig_y = 0
+
+    px_color = original_img[orig_y, orig_x, :]
     return px_color
 
+def calculate_triangles(img1_pts, img2_pts):
+    interpolated = interpolate_pointclouds(img1_pts, img2_pts, 0.5)
+    triangles = Delaunay(interpolated).simplices
+    return triangles
 
-def morph(img1, img2, img1_pts, img2_pts, tri, warp_frac, dissolve_frac):
+def morph(img1, img2, img1_pts, img2_pts, triangles, warp_frac, dissolve_frac):
     interpolated = interpolate_pointclouds(img1_pts, img2_pts, warp_frac)
-    triangulated = Delaunay(interpolated)
 
-##plt.triplot(interpolated[:,0], interpolated[:,1], triangulated.simplices)
+##plt.triplot(interpolated[:,0], interpolated[:,1], triangles)
 ##plt.plot(interpolated[:,0], interpolated[:,1], 'o')
-#    plt.triplot(points1[:,0], points1[:,1], triangulated.simplices)
+#    plt.triplot(points1[:,0], points1[:,1], triangles)
 #    plt.plot(points1[:,0], points1[:,1], 'o')
 #    plt.imshow(im1)
 #    plt.axis('equal')
 ##plt.gca().invert_yaxis()
 #    plt.show()
 
-    triangles_avg = create_triangles(interpolated, triangulated.simplices)
-    triangles_img1 = create_triangles(img1_pts, triangulated.simplices)
-    triangles_img2 = create_triangles(img2_pts, triangulated.simplices)
+    triangles_avg = create_triangles(interpolated, triangles)
+    triangles_img1 = create_triangles(img1_pts, triangles)
+    triangles_img2 = create_triangles(img2_pts, triangles)
 
     transformations_1toavg = calc_transform_all(triangles_img1, triangles_avg)
     transformations_2toavg = calc_transform_all(triangles_img2, triangles_avg)
-    print(transformations_1toavg.shape)
 
     intermed_img = np.zeros_like(img1)
     last_triangle_index = 0 #save last triangle because the next pixel is likely also in it
     transformation_1toavg = transformations_1toavg[last_triangle_index]
     transformation_2toavg = transformations_2toavg[last_triangle_index]
 
-    colors = [[255,0,0], [0,255,0], [0,0,255], [255,255,0], [0,255,255], [255,0,255]]
-    color = [255,0,0]
-    for y in range(intermed_img.shape[1]):
-        for x in range(intermed_img.shape[0]):
+#    for y in range(intermed_img.shape[1]):
+#        for x in range(intermed_img.shape[0]):
+    y_max = intermed_img.shape[1]
+    x_max = intermed_img.shape[0]
+    frac = 3
+    for y in range(int(y_max/frac), y_max - int(y_max/frac)):
+        for x in range(int(x_max/frac), x_max - int(x_max/frac)):
             pixel = intermed_img[y,x,:]
             if not (path.Path(triangles_avg[last_triangle_index]).contains_point([x,y])):
                 for t in range(len(triangles_avg)):
                     triangle_path = path.Path(triangles_avg[t])
                     if triangle_path.contains_point([x,y]):
-                        color = colors[t%len(colors)]
                         transformation_1toavg = transformations_1toavg[t]
                         transformation_2toavg = transformations_2toavg[t]
-                        last_triangle_index = t
-            intermed_img[y,x,:] = color
-            intermed_img[y,x,:] = calc_origin_pixel_color(img1, [x,y], transformation_1toavg)
+            intermed_img[y,x,:] = (1-dissolve_frac) * calc_origin_pixel_color(img1, [x,y], transformation_1toavg) + dissolve_frac * calc_origin_pixel_color(img2, [x,y], transformation_2toavg)
 
-    skio.imshow(intermed_img)
-    skio.show()
-
+    return intermed_img
 
 im1 = skio.imread("./faces/19-Rosalie.jpg")
-im2 = skio.imread("./faces/22-Eric.jpg")
+im2 = skio.imread("./faces/20-Luise.jpg")
 
 points1 = parse_pointfile(open("./points/19-Rosalie.txt").read())
 points1 = add_image_edge_points(points1, im1)
-points2 = parse_pointfile(open("./points/22-Eric.txt").read())
+points2 = parse_pointfile(open("./points/20-Luise.txt").read())
 points2 = add_image_edge_points(points2, im2)
 
 im1 = sk.img_as_float(im1)
 im2 = sk.img_as_float(im2)
 
-interpolated = interpolate_pointclouds(points1, points2, 0.5)
-triangulated = Delaunay(interpolated)
+warps = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+disve = [0.0, 0.1, 0.2, 0.3, 0.5, 0.5, 0.6, 0.7, 0.8]
+i = 0
+for warp_frac, dissolve_frac in zip(warps, disve): #linspace
+    print(i)
+    triangles = calculate_triangles(points1, points2)
 
-morphed = morph(im1, im2, points1, points2, triangulated, 0.5, 0.5)
+    morphed = morph(im1, im2, points1, points2, triangles, warp_frac, dissolve_frac)
+    skio.imshow(morphed)
+    skio.show()
+    skio.imsave("./sequence/" + str(i) + ".jpg", morphed)
+    i += 1
+    
 
-#triangle1 = np.array([ points1[triangulated.simplices[0][0]], points1[triangulated.simplices[0][1]], points1[triangulated.simplices[0][2]]])
-#triangle2 = np.array([ interpolated[triangulated.simplices[0][0]], interpolated[triangulated.simplices[0][1]], interpolated[triangulated.simplices[0][2]]])
-#print(triangle1.shape, triangle2.shape)
-#calc_affine_transform(triangle1, triangle2)
 
 
-#skio.imshow(im12)
-#skio.show()
 #uncomment to save image
-#skio.imsave("", im12)
